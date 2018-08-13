7d768128611243ca2fbaf1b5cd5f5f0fd33353bd
gutiantian123Abc <william.guxiangtian@gmail.com>
2018-08-13 13:51:20 -0400

    Added Socket.io connect and disconnect tracker

--- server/server.js
+++ server/server.js
@@ -1,11 +1,23 @@
 const path = require('path');
+const http = require('http');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 const express = require('express');
+const socketIO = require('socket.io');
 
 let app = express();
+let server = http.createServer(app);
+let io = socketIO(server);
+io.on('connection', (socket) => {
+    console.log('New user connected');
+    socket.on('disconnect', () => {
+        console.log('Disconnected from Server');
+    });
+});
+
 app.use(express.static(publicPath));
-app.listen(port, () => {
+
+server.listen(port, () => {
     console.log(`Server is up ${port}`);
 });
 
