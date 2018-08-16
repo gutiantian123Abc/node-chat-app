b402789fa0ad44c885e1316b03fc54cfb6792988
gutiantian123Abc <william.guxiangtian@gmail.com>
2018-08-16 08:05:01 -0400

    Add form messages and show incoming message in browser

--- public/js/index.js
+++ public/js/index.js
@@ -10,6 +10,21 @@ socket.on('disconnect', () => {
 
 socket.on('newMessage', (message) => {
     console.log('new message', message);
+    let li = jQuery('<li></li>');
+    li.text(`${message.from}: ${message.text}`);
+    jQuery('#message').append(li);
+});
+
+jQuery('#message-form').on('submit', (e) => {
+    e.preventDefault();
+
+    socket.emit('createMessage', {
+        from: 'User',
+        text: jQuery('[name=message]').val()
+    }, () => {
+
+    })
+
 });
 
 
